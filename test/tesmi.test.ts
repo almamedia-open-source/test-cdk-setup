aa0fae4f92a93809585bbf90c9bbdfdfdedb7466
Ari Palo <ari.palo@almamedia.fi>
2022-02-08 23:24:13 +0200

    Initial commit

---
+++ test/tesmi.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Tesmi from '../lib/tesmi-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tesmi-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Tesmi.TesmiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
